f425ecb624cf9a584a62425a923f7d03a12d8d98
Vui-Chee <vuicheesiew@gmail.com>
2021-07-19 10:07:34 +0800

    Update constants
    * remove window dimensions as they can change
    * set number of stars to render as a constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
+export const STARCOUNT = 2000
+
 export const RADIUS = 600 // Refers to radius of star cluster near bottom of canvas.
 
 export const FOV = 260
@@ -8,19 +10,14 @@ export const ANGLE = 0.01
 
 export const VERT = -0.001
 
-export const WINDOW_WIDTH = window.innerWidth
-
-export const SCALED_WINDOW_HEIGHT = window.innerHeight * 1.6 // Center cluster of stars near bottom.
-
 export const FRAMERATE = 50
 
 export default {
+  STARCOUNT,
   RADIUS,
   FOV,
   CRUISE,
   ANGLE,
   VERT,
-  WINDOW_WIDTH,
-  SCALED_WINDOW_HEIGHT,
   FRAMERATE,
 }
