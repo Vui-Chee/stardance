55efcfde2264873a1dd313f5bb1d66f9c11cddf2
Vui-Chee <vuicheesiew@gmail.com>
2021-07-19 10:08:16 +0800

    Setup window dimensions on star construction

--- src/lib/star.ts
+++ src/lib/star.ts
@@ -1,31 +1,27 @@
-import {
-  RADIUS,
-  WINDOW_WIDTH,
-  SCALED_WINDOW_HEIGHT,
-  FOV,
-  ANGLE,
-  VERT,
-  CRUISE,
-} from "./constants"
+import { STARCOUNT, RADIUS, FOV, ANGLE, VERT, CRUISE } from "./constants"
 
 export class Star {
   ctx: CanvasRenderingContext2D
   x: number
   y: number
   z: number
+  windowWidth: number
+  windowHeight: number
 
   constructor(ctx: CanvasRenderingContext2D) {
     this.ctx = ctx
     this.x = Math.random() * (2 * RADIUS) - RADIUS
     this.y = Math.random() * (2 * RADIUS) - RADIUS
     this.z = Math.random() * (2 * RADIUS) - RADIUS
+    this.windowWidth = window.innerWidth
+    this.windowHeight = window.innerHeight * 1.6
   }
 
   draw() {
     this.ctx.beginPath()
     let fovRatio = FOV / 2 / (FOV + this.z)
-    let newX = this.x * fovRatio + WINDOW_WIDTH / 2
-    let newY = this.y * fovRatio + SCALED_WINDOW_HEIGHT / 2
+    let newX = this.x * fovRatio + this.windowWidth / 2
+    let newY = this.y * fovRatio + this.windowHeight / 2
     let alpha = Math.min(Math.max(Math.abs(fovRatio), 0.1), 1)
     this.ctx.arc(newX, newY, Math.min(Math.abs(fovRatio), 3), 0, 360)
     this.ctx.fillStyle = `rgba(167,180,224,${alpha})`
@@ -35,10 +31,8 @@ export class Star {
   resetCoordinate(coord: number): number {
     if (coord < -RADIUS) {
       coord = RADIUS
-    } else {
-      if (coord > RADIUS) {
-        coord = -RADIUS // NEGATED radius.
-      }
+    } else if (coord > RADIUS) {
+      coord = -RADIUS // NEGATED radius.
     }
     return coord
   }
@@ -67,7 +61,7 @@ export function generateStars(
   ctx: CanvasRenderingContext2D
 ): Star[] {
   let stars: Star[] = []
-  let numStars = Math.min(2000, Math.round(2 * offsetWidth))
+  let numStars = Math.min(STARCOUNT, Math.round(2 * offsetWidth))
   for (let i = 0; i < numStars; i++) {
     stars.push(new Star(ctx))
   }
