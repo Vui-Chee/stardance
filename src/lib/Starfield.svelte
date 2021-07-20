eacd4c6adbe7b1c37e42eddc0c32f3fa72784bfd
Vui-Chee <vuicheesiew@gmail.com>
2021-07-20 14:36:39 +0800

    Initial theme switcher

--- src/lib/Starfield.svelte
+++ src/lib/Starfield.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
   import { onMount } from "svelte"
+
   import { FRAMERATE } from "./constants"
   import type { Star } from "./star"
   import { generateStars, renderStars } from "./star"
+
+  import ToggleTheme from "./ToggleTheme.svelte"
 
   // All states
   let stars: Star[] = []
   let canvas: HTMLCanvasElement
   let ctx: CanvasRenderingContext2D
+
+  // Theme related state
+  let isDark = true // control theme
+
+  let theme = {
+    primary: isDark ? "#262a36" : "#fb5508",
+    secondary: isDark ? "#101218" : "#fb6621",
+  }
+  $: cssVarStyles = Object.entries(theme)
+    .map(([key, value]) => `--${key}:${value}`)
+    .join(";")
+
+  function toggleTheme(isDark: boolean) {
+    if (isDark) {
+      theme = {
+        primary: "#262a36",
+        secondary: "#101218",
+      }
+    } else {
+      theme = {
+        primary: "#fb5508",
+        secondary: "#fb6621",
+      }
+    }
+  }
 
   // handle window resize
   function handleResize() {
@@ -21,7 +49,24 @@
     renderStars(stars)
   }
 
+  function handleMessage(event: CustomEvent) {
+    toggleTheme(event.detail.isDark)
+    if (event.detail.isDark) {
+      localStorage.setItem("theme", "dark")
+    } else {
+      localStorage.setItem("theme", "light")
+    }
+  }
+
   onMount(async () => {
+    let theme = localStorage.getItem("theme")
+    if (theme === "dark") {
+      isDark = true
+    } else {
+      isDark = false
+    }
+    toggleTheme(isDark)
+
     ctx = canvas.getContext("2d")
 
     // update canvas width/height to full screen
@@ -48,15 +93,29 @@
   })
 </script>
 
-<canvas id="starfield" bind:this={canvas} />
+<div id="starfield" style={cssVarStyles}>
+  <ToggleTheme {isDark} on:message={handleMessage} />
+  <canvas id="starfield" bind:this={canvas} />
+</div>
 <svelte:window on:resize={handleResize} />
 
 <style>
+  #starfield :global(.theme-switch-wrapper) {
+    position: fixed;
+    left: 10px;
+    top: 10px;
+    z-index: 10000;
+  }
+
   canvas#starfield {
     position: fixed;
     z-index: 1000;
     height: 100%;
     width: 100%;
-    background-image: -webkit-radial-gradient(bottom, #262a36, #101218);
+    background-image: -webkit-radial-gradient(
+      bottom,
+      var(--primary),
+      var(--secondary)
+    );
   }
 </style>
---
+++ src/lib/ToggleTheme.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte"
+
+  export let isDark = true
+
+  const dispatch = createEventDispatcher()
+
+  function switchTheme() {
+    dispatch("message", {
+      isDark,
+    })
+  }
+</script>
+
+<div class="theme-switch-wrapper">
+  <label class="theme-switch" for="checkbox">
+    <input
+      type="checkbox"
+      id="checkbox"
+      bind:checked={isDark}
+      on:change={switchTheme}
+    />
+    <div class="slider round" />
+  </label>
+</div>
+
+<style>
+  .theme-switch-wrapper {
+    display: flex;
+    align-items: center;
+  }
+  .theme-switch {
+    display: inline-block;
+    height: 34px;
+    position: relative;
+    width: 60px;
+  }
+
+  .theme-switch input {
+    display: none;
+  }
+
+  .slider {
+    background-color: #ccc;
+    bottom: 0;
+    cursor: pointer;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    background-color: #fff;
+    bottom: 4px;
+    content: "";
+    height: 26px;
+    left: 4px;
+    position: absolute;
+    transition: 0.4s;
+    width: 26px;
+  }
+
+  input:checked + .slider {
+    background-color: #66bb6a;
+  }
+
+  input:checked + .slider:before {
+    transform: translateX(26px);
+  }
+
+  .slider.round {
+    border-radius: 34px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
+  }
+</style>
